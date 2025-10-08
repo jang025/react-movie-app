4ccd083915fdd61fba073159357923a61a4ed9ab
Jeremy Ang <jang025@users.noreply.github.com>
2025-10-08 16:45:47 +0800

    feat: added feature where the add to favourites button is disabled after it is clicked
    remove: removed the alert function

--- react-movie-app/src/components/MovieCard.jsx
+++ react-movie-app/src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import { createFavourite } from "../services/airtableApiService";
+import { useState } from "react";
 
 const MovieCard = ({
   movie,
@@ -10,11 +11,15 @@ const MovieCard = ({
   original_language,
   release_date,
 }) => {
+  const [isFavourite, setIsFavourite] = useState(false);
   const handleAddFavourite = async function (event) {
-    // prevent page reloading
+    // prevent page reloading when i click on the button
     event.preventDefault();
+
+    // preventing users from adding the add to fav button more than once
+    if (isFavourite) return;
     await createFavourite(movie);
-    alert(`${title} added to favourites`);
+    setIsFavourite(true);
   };
   return (
     <Link to={`/movies/${id}`}>
@@ -34,7 +39,9 @@ const MovieCard = ({
             <p>{original_language ? original_language.toUpperCase() : "NA"}</p>
             <p>{release_date ? release_date.split("-")[0] : "NA"}</p>
           </div>
-          <button onClick={handleAddFavourite}>❤️ Add to Favourites</button>
+          <button onClick={handleAddFavourite} disabled={isFavourite}>
+            {isFavourite ? "❤️ Added" : "❤️ Add to Favourites"}
+          </button>
         </div>
       </div>
     </Link>
