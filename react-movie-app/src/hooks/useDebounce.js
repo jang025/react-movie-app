f674cc77e1b9bad54833c373a46308144fd4e9a1
Jeremy Ang <jang025@users.noreply.github.com>
2025-10-09 09:59:56 +0800

    style: added references to the useDebounce hook

--- react-movie-app/src/hooks/useDebounce.js
+++ react-movie-app/src/hooks/useDebounce.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+//! reference for deBounce() https://medium.com/@sankalpa115/usedebounce-hook-in-react-2c71f02ff8d8
+
 // reusable hook that allows me to implement debouncing in my search and preventing unneccessary API calls
 
 export function useDebounce(value, delay) {
